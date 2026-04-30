7d274b236bbb8d0df3a466ef8562b4523a53ab88
Erik Rodriguez <erik_rodriguez@ucsb.edu>
2026-04-29 17:36:40 -0700

    Format javascript for HelpRequest

--- frontend/src/fixtures/HelpRequestFixtures.js
+++ frontend/src/fixtures/HelpRequestFixtures.js
@@ -1,5 +1,15 @@
 const HelpRequestFixtures = {
-    oneHelpRequest: {
+  oneHelpRequest: {
+    id: 1,
+    requesterEmail: "erik_rodriguez@ucsb.edu",
+    teamId: "team4",
+    tableOrBreakoutRoom: "table",
+    requestTime: "2026-04-29T12:00:00",
+    explanation: "Help request for team 4 table",
+    solved: false,
+  },
+  threeHelpRequests: [
+    {
       id: 1,
       requesterEmail: "erik_rodriguez@ucsb.edu",
       teamId: "team4",
@@ -8,36 +18,25 @@ const HelpRequestFixtures = {
       explanation: "Help request for team 4 table",
       solved: false,
     },
-    threeHelpRequests: [
-      {
-        id: 1,
-        requesterEmail: "erik_rodriguez@ucsb.edu",
-        teamId: "team4",
-        tableOrBreakoutRoom: "table",
-        requestTime: "2026-04-29T12:00:00",
-        explanation: "Help request for team 4 table",
-        solved: false,
-      },
-      {
-        id: 5,
-        requesterEmail: "pconrad@ucsb.edu",
-        teamId: "team9",
-        tableOrBreakoutRoom: "breakout",
-        requestTime: "2026-04-29T19:30:00",
-        explanation: "Help request for team 9 breakout",
-        solved: false,
-      },
-      {
-        id: 6,
-        requesterEmail: "jane_doe@gmail.com",
-        teamId: "team10",
-        tableOrBreakoutRoom: "breakout",
-        requestTime: "2026-04-29T18:00:00",
-        explanation: "Help request for team 10 breakout",
-        solved: true,
-      },
-    ],
-  };
-  
-  export { HelpRequestFixtures };
-  
+    {
+      id: 5,
+      requesterEmail: "pconrad@ucsb.edu",
+      teamId: "team9",
+      tableOrBreakoutRoom: "breakout",
+      requestTime: "2026-04-29T19:30:00",
+      explanation: "Help request for team 9 breakout",
+      solved: false,
+    },
+    {
+      id: 6,
+      requesterEmail: "jane_doe@gmail.com",
+      teamId: "team10",
+      tableOrBreakoutRoom: "breakout",
+      requestTime: "2026-04-29T18:00:00",
+      explanation: "Help request for team 10 breakout",
+      solved: true,
+    },
+  ],
+};
+
+export { HelpRequestFixtures };
