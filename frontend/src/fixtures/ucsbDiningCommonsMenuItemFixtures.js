118d289d3ebc98048127b30346921143242d1cf1
Vanessa <vanessaxu@ucsb.edu>
2026-04-29 17:18:37 -0700

    Fixtures added

---
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
@@ -0,0 +1,30 @@
+const ucsbDiningCommonsMenuItemFixtures = {
+  oneMenuItem: {
+    id: 1,
+    diningCommonsCode: "Ortega",
+    name: "Pesto Pasta",
+    station: "Entree",
+  },
+  threeMenuItems: [
+    {
+      id: 1,
+      diningCommonsCode: "Ortega",
+      name: "Pesto Pasta",
+      station: "Entree",
+    },
+    {
+      id: 2,
+      diningCommonsCode: "DLG",
+      name: "Tri Tip Roast",
+      station: "Entree",
+    },
+    {
+      id: 3,
+      diningCommonsCode: "Portola",
+      name: "Sushi",
+      station: "Entree",
+    },
+  ],
+};
+
+export { ucsbDiningCommonsMenuItemFixtures };
