abcc8640e2e5c3d99f56c084f21712c34841402e
Erik Rodriguez <erik_rodriguez@ucsb.edu>
2026-05-01 11:44:49 -0700

    Revert external changes to version in main

---
+++ frontend/src/fixtures/ucsbOrganizationFixtures.js
@@ -0,0 +1,35 @@
+const ucsbOrganizationFixtures = {
+  oneUcsbOrganization: [
+    {
+      orgCode: "ACG",
+      orgTranslationShort: "ACG",
+      orgTranslation: "A Good Group",
+      inactive: true,
+    },
+  ],
+
+  threeUcsbOrganizations: [
+    {
+      orgCode: "ACG",
+      orgTranslationShort: "ACG",
+      orgTranslation: "A Good Group",
+      inactive: true,
+    },
+
+    {
+      orgCode: "AIES",
+      orgTranslationShort: "AIES",
+      orgTranslation: "AI & Entrepreneurship Society",
+      inactive: true,
+    },
+
+    {
+      orgCode: "BEI",
+      orgTranslationShort: "BEI",
+      orgTranslation: "Brain Exercise Initiative",
+      inactive: true,
+    },
+  ],
+};
+
+export { ucsbOrganizationFixtures };
