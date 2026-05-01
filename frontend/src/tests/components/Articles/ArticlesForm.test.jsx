92d723f8bf96519654b4e33a25ea9cdf13ef2b78
Vincent Nguyen <vincent150@ucsb.edu>
2026-04-30 21:46:21 -0700

    initial ArticlesForm and tests

---
+++ frontend/src/tests/components/Articles/ArticlesForm.test.jsx
@@ -0,0 +1,130 @@
+import { render, waitFor, fireEvent, screen } from "@testing-library/react";
+import ArticlesForm from "main/components/Articles/ArticlesForm";
+import { articlesFixtures } from "fixtures/articlesFixtures";
+import { BrowserRouter as Router } from "react-router";
+import { expect } from "vitest";
+
+const mockedNavigate = vi.fn(); 
+vi.mock("react-router", async () => {
+  const originalModule = await vi.importActual("react-router");
+  return {
+    ...originalModule,
+    useNavigate: () => mockedNavigate,
+  };
+});
+
+describe("ArticlesForm tests", () => {
+  test("renders correctly", async () => {
+    render(
+      <Router>
+        <ArticlesForm/>
+      </Router>,
+    );
+    await screen.findByText(/Title/);
+    await screen.findByText(/Create/);
+    expect(screen.getByText(/Title/)).toBeInTheDocument();
+  });
+
+  test("renders correctly when passing in an Article", async () => {
+    render(
+      <Router>
+        <ArticlesForm initialContents={articlesFixtures.oneArticle} />
+      </Router>,
+    );
+    await screen.findByTestId(/ArticlesForm-id/);
+    expect(screen.getByText(/Id/)).toBeInTheDocument();
+    expect(screen.getByTestId(/ArticlesForm-id/)).toHaveValue("1");
+  });
+
+  test("Correct Error messsages on bad input", async () => {
+    render(
+      <Router>
+        <ArticlesForm />
+      </Router>,
+    );
+    await screen.findByTestId("ArticlesForm-email");
+    const emailField = screen.getByTestId("ArticlesForm-email");
+    const urlField = screen.getByTestId("ArticlesForm-url");
+    const dateAddedField = screen.getByTestId("ArticlesForm-dateAdded");
+    const submitButton = screen.getByTestId("ArticlesForm-submit");
+
+    fireEvent.change(emailField, { target: { value: "bad-email" } });
+    fireEvent.change(urlField, { target: { value: "bad-url" } });
+    fireEvent.change(dateAddedField, { target: { value: "bad-date" } });
+    fireEvent.click(submitButton);
+
+    await screen.findByText(/Email must be in the format user@email.com/);
+    expect(
+      screen.getByText(/Date Added must be in ISO format/),
+    ).toBeInTheDocument();
+  });
+
+  test("Correct Error messsages on missing input", async () => {
+    render(
+      <Router>
+        <UCSBDateForm />
+      </Router>,
+    );
+    await screen.findByTestId("ArticlesForm-submit");
+    const submitButton = screen.getByTestId("ArticlesForm-submit");
+
+    fireEvent.click(submitButton);
+
+    await screen.findByText(/Title is required./);
+    expect(screen.getByText(/URL is required./)).toBeInTheDocument();
+    expect(screen.getByText(/Explanation is required./)).toBeInTheDocument();
+    expect(screen.getByText(/Email is required./)).toBeInTheDocument();
+    expect(screen.getByText(/DateAdded is required./)).toBeInTheDocument();
+  });
+
+  test("No Error messsages on good input", async () => {
+    const mockSubmitAction = vi.fn();
+
+    render(
+      <Router>
+        <ArticlesForm submitAction={mockSubmitAction} />
+      </Router>,
+    );
+    await screen.findByTestId("ArticlesForm-title");
+
+    const titleField = screen.getByTestId("ArticlesForm-title");
+    const urlField = screen.getByTestId("ArticlesForm-url");
+    const explanationField = screen.getByTestId("ArticlesForm-explanation");
+    const emailField = screen.getByTestId("ArticlesForm-email");
+    const dateAddedField = screen.getByTestId("ArticlesForm-dateAdded");
+    const submitButton = screen.getByTestId("ArticlesForm-submit");
+
+    fireEvent.change(titleField, { target: { value: "Test Title" } });
+    fireEvent.change(urlField, { target: { value: "https://test.com" } });
+    fireEvent.change(explanationField, { target: { value: "Test Explanation" } });
+    fireEvent.change(emailField, { target: { value: "test@email.com" } });
+    fireEvent.change(dateAddedField, { target: { value: "2022-01-02T12:00:00" } });
+    fireEvent.click(submitButton);
+
+    await waitFor(() => expect(mockSubmitAction).toHaveBeenCalled());
+
+    expect(
+      screen.queryByText(/URL must be in the format https:\/\/.../),
+    ).not.toBeInTheDocument();
+    expect(
+      screen.queryByText(/Email must be in the format user@email.com/),
+    ).not.toBeInTheDocument();
+    expect(
+      screen.queryByText(/Date Added must be in ISO format/),
+    ).not.toBeInTheDocument();
+  });
+
+  test("that navigate(-1) is called when Cancel is clicked", async () => {
+    render(
+      <Router>
+        <ArticlesForm />
+      </Router>,
+    );
+    await screen.findByTestId("ArticlesForm-cancel");
+    const cancelButton = screen.getByTestId("ArticlesForm-cancel");
+
+    fireEvent.click(cancelButton);
+
+    await waitFor(() => expect(mockedNavigate).toHaveBeenCalledWith(-1));
+  });
+});
