303f713ceec64ba7695182280ed17a1bbd2d78d8
Tanvi Ladha <tladha@ucsb.edu>
2026-04-30 17:29:06 -0700

    MenuItemReview fixtures with correct format

--- frontend/src/fixtures/menuItemReviewFixtures.js
+++ frontend/src/fixtures/menuItemReviewFixtures.js
@@ -8,33 +8,33 @@
 
 const menuItemReviewFixtures = {
   oneReview: {
-    itemId: 1, 
-    reviewerEmail: "tladha@ucsb.edu", 
+    itemId: 1,
+    reviewerEmail: "tladha@ucsb.edu",
     stars: 5,
     dateReviewed: "2026-04-30T12:00:00",
-    comments: "Super yummy!"
+    comments: "Super yummy!",
   },
   threeReviews: [
     {
-      itemId: 1, 
-      reviewerEmail: "tladha@ucsb.edu", 
+      itemId: 1,
+      reviewerEmail: "tladha@ucsb.edu",
       stars: 5,
       dateReviewed: "2026-04-30T12:00:00",
-      comments: "Super yummy!"
+      comments: "Super yummy!",
     },
     {
-      itemId: 2, 
-      reviewerEmail: "tladha@ucsb.edu", 
+      itemId: 2,
+      reviewerEmail: "tladha@ucsb.edu",
       stars: 4,
       dateReviewed: "2026-04-31T01:00:00",
-      comments: "Pretty good..."
+      comments: "Pretty good...",
     },
     {
-      itemId: 3, 
-      reviewerEmail: "tladha@ucsb.edu", 
+      itemId: 3,
+      reviewerEmail: "tladha@ucsb.edu",
       stars: 2,
       dateReviewed: "2026-04-29T07:00:00",
-      comments: "Not great"
+      comments: "Not great",
     },
   ],
 };
