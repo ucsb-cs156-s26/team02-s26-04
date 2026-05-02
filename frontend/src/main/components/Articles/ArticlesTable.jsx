547be9fd7e383717296a601d8802c30d1d205193
Vincent Nguyen <vincent150@ucsb.edu>
2026-05-01 20:11:25 -0700

    articles table component + tests

---
+++ frontend/src/main/components/Articles/ArticlesTable.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import OurTable, { ButtonColumn } from "main/components/OurTable";
+
+import { useBackendMutation } from "main/utils/useBackend";
+import {
+  cellToAxiosParamsDelete,
+  onDeleteSuccess,
+} from "main/utils/ArticlesUtils";
+import { useNavigate } from "react-router";
+import { hasRole } from "main/utils/useCurrentUser";
+
+export default function ArticlesTable({ dates, currentUser }) {
+  const navigate = useNavigate();
+
+  const editCallback = (cell) => {
+    navigate(`/articles/edit/${cell.row.original.id}`);
+  };
+
+  // Stryker disable all : hard to test for query caching
+
+  const deleteMutation = useBackendMutation(
+    cellToAxiosParamsDelete,
+    { onSuccess: onDeleteSuccess },
+    ["/api/articles/all"],
+  );
+  // Stryker restore all
+
+  // Stryker disable next-line all : TODO try to make a good test for this
+  const deleteCallback = async (cell) => {
+    deleteMutation.mutate(cell);
+  };
+
+  const columns = [
+    {
+      header: "id",
+      accessorKey: "id", // accessor is the "key" in the data
+    },
+    {
+      header: "title",
+      accessorKey: "title",
+    },
+    {
+      header: "url",
+      accessorKey: "url",
+    },
+    {
+      header: "explanation",
+      accessorKey: "explanation",
+    },
+    {
+      header: "email",
+      accessorKey: "email",
+    },
+    {
+      header: "dateAdded",
+      accessorKey: "dateAdded",
+    },
+  ];
+
+  if (hasRole(currentUser, "ROLE_ADMIN")) {
+    columns.push(
+      ButtonColumn("Edit", "primary", editCallback, "ArticlesTable"),
+    );
+    columns.push(
+      ButtonColumn("Delete", "danger", deleteCallback, "ArticlesTable"),
+    );
+  }
+
+  return <OurTable data={dates} columns={columns} testid={"ArticlesTable"} />;
+}
