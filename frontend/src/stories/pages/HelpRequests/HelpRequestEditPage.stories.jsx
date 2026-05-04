91f6a65e8cf8b88233a27febc9e6535837c4677b
Erik Rodriguez <erik_rodriguez@ucsb.edu>
2026-05-04 14:08:43 -0700

    Create HelpRequest edit page, stories, and tests

---
+++ frontend/src/stories/pages/HelpRequests/HelpRequestEditPage.stories.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
+import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
+import { helpRequestFixtures } from "fixtures/helpRequestFixtures";
+import { http, HttpResponse } from "msw";
+
+import HelpRequestEditPage from "main/pages/HelpRequests/HelpRequestEditPage";
+
+export default {
+  title: "pages/HelpRequests/HelpRequestEditPage",
+  component: HelpRequestEditPage,
+};
+
+const Template = () => <HelpRequestEditPage storybook={true} />;
+
+export const Default = Template.bind({});
+Default.parameters = {
+  msw: [
+    http.get("/api/currentUser", () => {
+      return HttpResponse.json(apiCurrentUserFixtures.userOnly, {
+        status: 200,
+      });
+    }),
+    http.get("/api/systemInfo", () => {
+      return HttpResponse.json(systemInfoFixtures.showingNeither, {
+        status: 200,
+      });
+    }),
+    http.get("/api/helprequests", () => {
+      return HttpResponse.json(helpRequestFixtures.threeHelpRequests[0], {
+        status: 200,
+      });
+    }),
+    http.put("/api/helprequests", () => {
+      return HttpResponse.json({}, { status: 200 });
+    }),
+    http.put("/api/helprequests", (req) => {
+      window.alert("PUT: " + req.url + " and body: " + req.body);
+      return HttpResponse.json({}, { status: 200 });
+    }),
+  ],
+};
