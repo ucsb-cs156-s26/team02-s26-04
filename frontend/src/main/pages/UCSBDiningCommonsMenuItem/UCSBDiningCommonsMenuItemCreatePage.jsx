b8521169a5b3069255b02bb7f11abc60f829d33d
Vanessa <vanessaxu@ucsb.edu>
2026-05-05 23:01:22 -0700

    repush to main

--- frontend/src/main/pages/UCSBDiningCommonsMenuItem/UCSBDiningCommonsMenuItemCreatePage.jsx
+++ frontend/src/main/pages/UCSBDiningCommonsMenuItem/UCSBDiningCommonsMenuItemCreatePage.jsx
@@ -1,4 +1,46 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+<<<<<<< HEAD
+=======
+import UCSBDiningCommonsMenuItemForm from "main/components/UCSBDiningCommonsMenuItem/UCSBDiningCommonsMenuItemForm";
+import { Navigate } from "react-router";
+import { useBackendMutation } from "main/utils/useBackend";
+import { toast } from "react-toastify";
+
+export default function UCSBDiningCommonsMenuItemCreatePage({
+  storybook = false,
+}) {
+  const objectToAxiosParams = (UCSBDiningCommonsMenuItem) => ({
+    url: "/api/ucsbdiningcommonsmenuitem/post",
+    method: "POST",
+    params: {
+      name: UCSBDiningCommonsMenuItem.name,
+      diningCommonsCode: UCSBDiningCommonsMenuItem.diningCommonsCode,
+      station: UCSBDiningCommonsMenuItem.station,
+    },
+  });
+
+  const onSuccess = (UCSBDiningCommonsMenuItem) => {
+    toast(
+      `New Dining Commons Menu Item Created - id: ${UCSBDiningCommonsMenuItem.id} name: ${UCSBDiningCommonsMenuItem.name}`,
+    );
+  };
+
+  const mutation = useBackendMutation(
+    objectToAxiosParams,
+    { onSuccess },
+    ["/api/ucsbdiningcommonsmenuitem/all"], 
+  );
+
+  const { isSuccess } = mutation;
+
+  const onSubmit = async (data) => {
+    mutation.mutate(data);
+  };
+
+  if (isSuccess && !storybook) {
+    return <Navigate to="/ucsbdiningcommonsmenuitem" />;
+  }
+>>>>>>> 66b488c (repush to main)
 
 export default function UCSBDiningCommonsMenuItemCreatePage() {
   // Stryker disable all : placeholder for future implementation
